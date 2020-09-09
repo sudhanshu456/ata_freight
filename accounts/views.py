3b640d2217e7f90ffeda035100bc9b919a0b5090
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-09 20:37:22 +0530

    added view for sign up with roles

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import PublicUserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.models import Group, Permission
 # Create your views here.
 
 
@@ -10,3 +11,12 @@ class SignUpView(generic.CreateView):
     form_class = PublicUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        '''adding roles while signup'''
+        print(form.instance.roles)
+        user = form.save()
+        group, created = Group.objects.get_or_create(name=form.instance.roles) 
+        user.groups.add(group)
+        return super().form_valid(form)
+
