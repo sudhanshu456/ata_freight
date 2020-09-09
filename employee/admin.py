12616419733c2c5f542cd07767434ab11e4ffb7e
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-09 20:37:37 +0530

    registered models in admin site

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Employee,leave_application
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(leave_application)
