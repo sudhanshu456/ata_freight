81ba156bc1df1588165687b2d4a222274a726fb8
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-10 10:38:11 +0530

    customization in forms of leave

--- employee/forms.py
+++ employee/forms.py
@@ -3,10 +3,27 @@ from .models import leave_application
 from ckeditor.widgets import CKEditorWidget
 
 class leave_applications(forms.ModelForm):
+    
+
     class Meta:
-        model=leave_applications
+        model=leave_application
         fields='__all__'
+        exclude=['user','applied_on','approved','total_days']
+
+
+
+
+
+        
         # widgets = {
 	    #     'title': forms.TextInput(attrs={'class': 'input','placeholder':'Title'})
         # }
         
+    # start_date = forms.DateField(
+    #     widget=forms.DateInput(format='%m/%d/%Y', attrs={'class': 'datepicker'}),
+    #     input_formats=('%m/%d/%Y', )
+    #     )
+    # end_date=forms.DateField(
+    #     widget=forms.DateInput(format='%m/%d/%Y', attrs={'class': 'datepicker'}),
+    #     input_formats=('%m/%d/%Y', )
+    #     )
