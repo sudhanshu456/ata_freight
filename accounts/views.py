ee65fb812bf24ca421307b5eca67304e488803d3
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-10 10:37:45 +0530

    added permissions

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,28 @@ class SignUpView(generic.CreateView):
     def form_valid(self, form):
         '''adding roles while signup'''
         user = form.save()
-        group, created = Group.objects.get_or_create(name=form.instance.roles) 
+        
+        rol=form.instance.roles
+        group, created = Group.objects.get_or_create(name=rol) 
+        if rol=='manager':
+            p1=Permission.objects.get(name='Can view employee')
+            p2=Permission.objects.get(name='Can change employee')
+            group.permissions.add(p1,p2)
+        elif rol=='employee':
+            p1=Permission.objects.get(name='Can view employee')
+            p2=Permission.objects.get(name='Can change employee')
+            p3=Permission.objects.get(name='Can add employee')
+            group.permissions.add(p1,p2,p3)
+        elif rol=='admin':
+             p=Permission.objects.get(name='Can view employee')
+             p1=Permission.objects.get(name='Can view permission')
+             p2=Permission.objects.get(name='Can view group')
+             p3=Permission.objects.get(name='Can view leave_application')
+             p4=Permission.objects.get(name='Can view user')
+             p5=Permission.objects.get(name='Can change leave_application')
+             group.permissions.add(p1,p2,p3,p,p5,p4)
+             user.is_staff=True
+             user.save() 
         user.groups.add(group)
         if form.instance.roles=='employee':
             Employee.objects.create(
