097ea9aee4e2dc85feb4f2c56ce5acc7d972261d
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-10 10:39:55 +0530

    view for list and details

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,reverse
+from employee.models import leave_application,Employee
 # Create your views here.
+from employee.models import leave_application,Employee
+from django.views.generic import ListView,DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.http import HttpResponseRedirect
+
+class approve_list(LoginRequiredMixin,ListView):
+     model = leave_application
+     template_name='approve_list.html'
+
+     # def test_func(self):
+     #      return self.request.user.roles == "manager"
+
+
+class ApplicationDetail(DetailView):
+    model = leave_application
+    template_name = 'detailapplication.html'
+
+
+
+def approve_leave(request,id):
+     leve=leave_application.objects.get(id=id)
+     usr=Employee.objects.get(user=leve.user)
+     usr.num_available_leave=usr.num_available_leave-leve.total_days
+     usr.save()
+     leve.approved=True
+     leve.save()
+     return HttpResponseRedirect(reverse('application_detail', args=[id]))
